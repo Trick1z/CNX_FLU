99dcdc66f77eac2da8916ea6ec268bc9b83f8e3b
trick1z <tiker.zone.teen@gmail.com>
2025-07-30 14:17:07 +0700

    เพิ่ม model   WordScore

---
+++ Models/userData.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace JwtSqlDemo.Models
+
+{
+	[Table("WordScore")] 
+	public class WordScore
+	{
+		[Key]   // ✅ บอก EF Core ว่านี่คือ Primary Key
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)] // auto increment
+		public int WordId { get; set; } = 0;
+
+
+		public int UserId { get; set; } = 0;
+		public string Word { get; set; } = string.Empty;
+		public int Score { get; set; }  = 0 ;
+
+
+		[Column("Create_time")]  
+		public DateTime CreateTime { get; set; }
+	}
+
+	//public class WordScoreFilter
+	//{
+	//	public int UserId { get; set; } = 0 ;
+	//	public string DateTime Date { get; set; } = string.Empty;
+	//}
+
+}
