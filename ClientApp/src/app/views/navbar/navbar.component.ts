4e044acb385b3836302097e6b28ff4f2d738e047
trick1z <tiker.zone.teen@gmail.com>
2025-07-30 11:20:55 +0700

    ใส่ username navbar

--- ClientApp/src/app/views/navbar/navbar.component.ts
+++ ClientApp/src/app/views/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,11 +9,24 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  constructor(
+export class NavbarComponent implements OnInit {
+  Username: string = 'Unknown User';
+  ngOnInit(): void {
+    this.setUser();
+  }
+  constructor(private route: Router) {}
 
-    private route : Router
-  ){}
+  setUser() {
+    const data = sessionStorage.getItem('user');
+
+    if (data !== null) {
+      const parsed = JSON.parse(data);
+      // this.Username = parsed.
+      this.Username = parsed.user.username;
+    } else {
+      // กรณี data เป็น null (เช่น ไม่มี key ใน sessionStorage)
+    }
+  }
   onLogout() {
     sessionStorage.clear();
     return this.route.navigate(['/login']);
