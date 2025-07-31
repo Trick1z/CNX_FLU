e24b59fafa6340b70944e9fb9f70416e51808103
trick1z <tiker.zone.teen@gmail.com>
2025-07-31 16:47:31 +0700

    แก้ AppDbContext ปรับ DbSet<WordScore>

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,10 +8,8 @@ namespace JwtSqlDemo.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
-        //public DbSet<UserData> UserData { get; set; }
-
         public DbSet<WordScore> WordScore { get; set; }
-        //public DbSet<InsertWordScore> InsertWordScore { get; set; }
+        //public DbSet<UserData> UserData { get; set; }
 
 
         public DbSet<Test> vw_getUser { get; set; }
