80f515c73a2d6bef4373d9b2ed152d1087ee25e4
trick1z <tiker.zone.teen@gmail.com>
2025-08-01 16:51:14 +0700

    fix ng oninit for ang 16

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,39 +1,77 @@
+// import { Component, OnInit } from '@angular/core';
+// import { NavigationEnd, Route, Router, RouterOutlet } from '@angular/router';
+// import { NavbarComponent } from './views/navbar/navbar.component';
+// import { NgIf } from '@angular/common';
+// import { filter } from 'rxjs';
+
+// // import { filter } from 'rxjs/operators';
+
+
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, NavbarComponent, NgIf],
+//   templateUrl: './app.component.html',
+//   // styleUrl: './app.component.scss',
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent implements OnInit {
+
+//   showNavbar: boolean = false;
+//   // ngOnInit(): void {
+//   //   this.router.events.pipe(
+//   //     filter(event => event instanceof NavigationEnd)
+//   //   ).subscribe((event: NavigationEnd) => {
+//   //     // แสดง navbar เฉพาะหน้า /games
+//   //     this.showNavbar = event.urlAfterRedirects !== '/login';
+//   //   });
+//   // }
+
+//   ngOnInit() {
+//     this.router.events
+//       .pipe(filter(event => event instanceof NavigationEnd))
+//       .subscribe((event: NavigationEnd) => {
+//         const hiddenRoutes = ['/','/login', '/register'];
+//         this.showNavbar = !hiddenRoutes.includes(event.urlAfterRedirects);
+//       });
+//   }
+
+//   constructor(private router: Router) {}
+
+//   title = 'app';
+
+// }
+
+
+// up for 19
+// down for 16
+
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Route, Router, RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from './views/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   showNavbar: boolean = false;
-  // ngOnInit(): void {
-  //   this.router.events.pipe(
-  //     filter(event => event instanceof NavigationEnd)
-  //   ).subscribe((event: NavigationEnd) => {
-  //     // แสดง navbar เฉพาะหน้า /games
-  //     this.showNavbar = event.urlAfterRedirects !== '/login';
-  //   });
-  // }
-
-  ngOnInit() {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        const hiddenRoutes = ['/','/login', '/register'];
-        this.showNavbar = !hiddenRoutes.includes(event.urlAfterRedirects);
-      });
-  }
 
   constructor(private router: Router) {}
 
-  title = 'app';
-
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(event => {
+        const hiddenRoutes = ['/', '/login', '/register'];
+        this.showNavbar = !hiddenRoutes.includes(event.urlAfterRedirects);
+      });
+  }
 }
